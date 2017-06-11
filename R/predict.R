fe1654a632076bea19f98bf844a8fd3b391fa2c2
hutaobo <thuac@ust.hk>
2017-06-11 20:17:59 +0800

    Copy number variation analysis based on AluScan sequences

--- R/predict.R
+++ R/predict.R
@@ -10,25 +10,27 @@ predictTumor <- function(file_path) {
   file <- read.table(file_path, header = TRUE, stringsAsFactors = FALSE)
   cnv <- data.frame(chr = file$chromosome, start = file$start, end = file$end, cnv = ifelse(file$p.value >= 0.01, 0, ifelse(file$zScore > 0, 1, -1)), stringsAsFactors = FALSE)
   cnv_gr <- makeGRangesFromDataFrame(cnv, keep.extra.columns = TRUE)
+  seqlevelsStyle(cnv_gr) <- "UCSC"
   load("recurr_cnv.RData", verbose = FALSE)  # load the GenomicRanges 'recurr_cnv'
   feature <- subsetByOverlaps(cnv_gr, recurr_cnv)
   feature <- as.data.frame(feature)
-  rownames(feature) <- with(feature, paste0("chr", seqnames, ":", start, "-", end))
+  rownames(feature) <- with(feature, paste0(seqnames, ":", start, "-", end))
   feature <- feature[, "cnv", drop = FALSE]
   feature <- data.frame(t(feature), stringsAsFactors = FALSE)
   load("fit.RData", verbose = FALSE)  # load the prediction model 'fit'
   pred <- predict(fit, feature, type = "class")
   if(pred == "tumor") {
+    result <- TRUE
     cat("\n",
         "#############################################\n",
         "# This blood sample is from Cancer patient. #\n",
         "#############################################\n")
-    return(TRUE)
   } else if (pred == "control") {
+    result <- FALSE
     cat("\n",
         "#################################################\n",
         "# This blood sample is from Non-Cancer patient. #\n",
         "#################################################\n")
-    return(FALSE)
   }
+  return(result)
 }
