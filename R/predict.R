eea6a9c55c1c7a5ea2fe2d6d5472bfdaa8a82f35
hutaobo <thuac@ust.hk>
2017-06-11 21:03:43 +0800

    Copy number variation analysis based on AluScan sequences

--- R/predict.R
+++ R/predict.R
@@ -11,14 +11,12 @@ predictTumor <- function(file_path) {
   cnv <- data.frame(chr = file$chromosome, start = file$start, end = file$end, cnv = ifelse(file$p.value >= 0.01, 0, ifelse(file$zScore > 0, 1, -1)), stringsAsFactors = FALSE)
   cnv_gr <- makeGRangesFromDataFrame(cnv, keep.extra.columns = TRUE)
   seqlevelsStyle(cnv_gr) <- "UCSC"
-  load("recurr_cnv.RData", verbose = FALSE)  # load the GenomicRanges 'recurr_cnv'
-  feature <- subsetByOverlaps(cnv_gr, recurr_cnv)
+  feature <- subsetByOverlaps(cnv_gr, recurr_cnv) # 'recurr_cnv' is the GenomicRanges
   feature <- as.data.frame(feature)
   rownames(feature) <- with(feature, paste0(seqnames, ":", start, "-", end))
   feature <- feature[, "cnv", drop = FALSE]
   feature <- data.frame(t(feature), stringsAsFactors = FALSE)
-  load("fit.RData", verbose = FALSE)  # load the prediction model 'fit'
-  pred <- predict(fit, feature, type = "class")
+  pred <- predict(fit, feature, type = "class") # 'fit' is the prediction model
   if(pred == "tumor") {
     result <- TRUE
     cat("\n",
