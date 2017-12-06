cd9f2262bd4f331a44e801020c2e25ed58d2e324
hutaobo <thuac@ust.hk>
2017-12-06 14:16:28 +0800

    Copy number variation analysis based on AluScan sequences

--- R/seg.R
+++ R/seg.R
@@ -11,17 +11,17 @@ seg2CNV <- function(seg.list, return = c('df', 'gr')) {
   for (i in seq_along(seg.list)) {
     sample_name <- strsplit(basename(seg.list[i]), '[.]')[[1]][1]
     file <- fread(seg.list[i], header = TRUE, stringsAsFactors = FALSE, data.table = FALSE)
-    cnv <- data.frame(chr = file$chromosome, start = file$start, end = file$end, cnv = ifelse(file$p.value >= 0.01, "N", ifelse(file$zScore > 0, "G", "L")), stringsAsFactors = FALSE)
+    cnv <- data.frame(chr = file$chromosome, start = file$start, end = file$end, cnv = ifelse(file$p.value >= 0.01, 0, ifelse(file$zScore > 0, 1, -1)), stringsAsFactors = FALSE)
     colnames(cnv)[4] <- sample_name
     if (i == 1) {
       final_cnv <- cnv
     } else {
-      final_cnv <- merge(final_cnv, cnv, all = TRUE, sort = FALSE)
+      final_cnv <- merge(final_cnv, cnv, all = FALSE, sort = FALSE)
     }
   }
-  final_cnv[is.na(final_cnv)] <- "N"
+  final_cnv[is.na(final_cnv)] <- 0
   # add 'recurrence' column
-  final_cnv$recurrence <- rowSums(final_cnv == "G") + rowSums(final_cnv == "L")
+  final_cnv$recurrence <- rowSums(final_cnv == 1) + rowSums(final_cnv == -1)
   if (!grepl("chr", final_cnv[1, 1])) {
     final_cnv$chr <- paste0("chr", final_cnv$chr)
   }
